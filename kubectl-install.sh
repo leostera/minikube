202fe75e322e0bc4f5d66b39fc3aa15f15096648
Leandro Ostera <leandro@ostera.io>
2016-11-25 07:25:52 +0100

    Ooops! Should be downloading darwin

--- kubectl-install.sh
+++ kubectl-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -O https://storage.googleapis.com/kubernetes-release/release/v1.4.3/bin/linux/amd64/kubectl
+curl -O https://storage.googleapis.com/kubernetes-release/release/v1.4.3/bin/darwin/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 kubectl version
