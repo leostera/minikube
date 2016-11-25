e18cbd56628e025a242af90252a4890b20092d55
Leandro Ostera <leandro@ostera.io>
2016-11-25 23:47:31 +0100

    Share docker with minikube

--- scripts/minikube-configure.sh
+++ scripts/minikube-configure.sh
@@ -5,3 +5,4 @@ minikube config set cpus $(( `sysctl -n hw.ncpu` - 1 ))
 minikube config set memory 8000
 minikube start \
   --iso-url https://github.com/coreos/minikube-iso/releases/download/v0.0.5/minikube-v0.0.5.iso
+eval $(minikube docker-env)
