49ab36df24e5f862a3a2080287525c1ea6c4899e
Leandro Ostera <leandro@ostera.io>
2016-11-25 21:15:03 +0100

    Drop rkt and use sysctl to get cpu count instead

--- scripts/minikube-configure.sh
+++ scripts/minikube-configure.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 minikube config set vm-driver xhyve
-minikube config set cpus $(( `getconf _NPROCESSORS_ONLN` - 1 ))
+minikube config set cpus $(( `sysctl -n hw.ncpu` - 1 ))
 minikube config set memory 8000
 minikube start \
-  --container-runtime=rkt \
   --iso-url https://github.com/coreos/minikube-iso/releases/download/v0.0.5/minikube-v0.0.5.iso
