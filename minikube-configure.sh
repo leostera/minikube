ba56f50a9683d9ddc7ca41b39ba1d0a7baf42138
Leandro Ostera <leandro@ostera.io>
2016-11-25 07:20:13 +0100

    Start one

--- minikube-configure.sh
+++ minikube-configure.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 minikube config set vm-driver xhyve
+minikube start \
+  --container-runtime=rkt \
+  --iso-url https://github.com/coreos/minikube-iso/releases/download/v0.0.5/minikube-v0.0.5.iso
