095d00e93a1378611ad28eff2a3b65481ca15678
Leandro Ostera <leandro@ostera.io>
2016-11-26 12:51:23 +0100

    Move default static to apps namespace

--- scripts/dnsmasq-configure.sh
+++ scripts/dnsmasq-configure.sh
@@ -3,4 +3,6 @@
 echo address=/dev/`minikube ip` > /usr/local/etc/dnsmasq.conf
 sudo brew services restart dnsmasq
 sudo mkdir -p /etc/resolver
+echo nameserver 127.0.0.1 | sudo tee /etc/resolver/`hostname -s`
 echo nameserver 127.0.0.1 | sudo tee /etc/resolver/dev
+
